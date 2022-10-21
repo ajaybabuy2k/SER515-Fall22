8770c92072f6c944d69d5cc2b0c075207957f593
ajaybabuy2k <ayadava1@asu.edu>
2022-10-20 23:28:34 -0700

    implemented all the design patterns and updated

--- composition/Facade.java
+++ composition/Facade.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Scanner;
 
 public class Facade {
@@ -26,47 +25,6 @@ public class Facade {
 			System.exit(0);
 		}
 
-		// Implementing iterator pattern
-
-		//Traversing through the product list using the iterator pattern
-
-		System.out.println("\nImplementing the iterator pattern \n");
-
-		System.out.println("The product list as follows\n");
-
-		ArrayList<String> productMenuList= new ArrayList<>();
-		productMenuList.add("Meat:Beef");
-		productMenuList.add("Meat:Pork");
-		productMenuList.add("Meat:Mutton");
-		productMenuList.add("Produce:Tomato");
-		productMenuList.add("Produce:Onion");
-
-		Iterator iter = productMenuList.iterator();
-        ProductIterator productIterator = new ProductIterator();
-		while(iter.hasNext()) {
-			System.out.println(productIterator.next(iter));
-		}
-
-		System.out.println("The user product list as follows\n");
-
-		ArrayList<String> userProductMenuList= new ArrayList<>();
-		userProductMenuList.add("tutu:Beef");
-		userProductMenuList.add("tutu:Tomato");
-		userProductMenuList.add("mimi:Beef");
-		userProductMenuList.add("pepe:Beef");
-		userProductMenuList.add("pepe:Tomato");
-		userProductMenuList.add("pepe:Onion");
-
-
-		iter = userProductMenuList.iterator();
-		OfferingIterator offeringIterator = new OfferingIterator();
-		while(iter.hasNext()) {
-			System.out.println(offeringIterator.next(iter));
-		}
-
-		// Implementing bridge pattern
-
-		System.out.println("\nImplementing the bridge pattern\n");
 
 		String userMenuOption="Not Avail";
 
@@ -74,6 +32,48 @@ public class Facade {
 
 		while(!userMenuOption.equals("quit"))
 		{
+			// Implementing iterator pattern
+
+			//Traversing through the product list using the iterator pattern
+
+			System.out.println("\nImplementing the iterator pattern \n");
+
+			System.out.println("The product list as follows\n");
+
+			ArrayList<String> productMenuList= new ArrayList<>();
+			productMenuList.add("Meat:Beef");
+			productMenuList.add("Meat:Pork");
+			productMenuList.add("Meat:Mutton");
+			productMenuList.add("Produce:Tomato");
+			productMenuList.add("Produce:Onion");
+
+			Iterator iter = productMenuList.iterator();
+			ProductIterator productIterator = new ProductIterator();
+			while(iter.hasNext()) {
+				System.out.println(productIterator.next(iter));
+			}
+
+			System.out.println("The user product list as follows\n");
+
+			ArrayList<String> userProductMenuList= new ArrayList<>();
+			userProductMenuList.add("tutu:Beef");
+			userProductMenuList.add("tutu:Tomato");
+			userProductMenuList.add("mimi:Beef");
+			userProductMenuList.add("pepe:Beef");
+			userProductMenuList.add("pepe:Tomato");
+			userProductMenuList.add("pepe:Onion");
+
+
+			iter = userProductMenuList.iterator();
+			OfferingIterator offeringIterator = new OfferingIterator();
+			while(iter.hasNext()) {
+				System.out.println(offeringIterator.next(iter));
+			}
+
+			// Implementing bridge pattern
+
+			System.out.println("\nImplementing the bridge pattern\n");
+
 			System.out.println("\nSelect from Product Menu Type \nmeat or produce\nType meat for meat Product & produce for produce Product \n ");
 			userMenuOption=sc.next();
 			if(userMenuOption.equalsIgnoreCase("meat") || userMenuOption.equalsIgnoreCase("produce"))
@@ -82,7 +82,25 @@ public class Facade {
 				productMenuIterator.displayUserChoice(userMenuOption);
 			}
 
+			System.out.println("Enter quit to order the dish");
+			System.out.println("Do you want to quit yes or no");
+
+			String input_user=sc.next();
+
+			if(input_user.equalsIgnoreCase("yes"))
+			{
+				System.exit(0);
+			}
+
+
 		}
+
+
+		System.out.println("Implementing the visitor pattern!!\n");
+
+		int bidValue = addTrading(new Trading());
+
+
 
 	}
 
@@ -93,8 +111,13 @@ public class Facade {
 
 	}
 
-	public void addTrading() {
+	public int addTrading(Trading object) throws IOException{
 
+		int bidValue=object.bidTradingValue();
+
+		System.out.println("bidValue");
+
+		return 0;
 	}
 
 	public void viewTrading() {
--- composition/Trading.java
+++ composition/Trading.java
@@ -1,3 +1,8 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.File;
+
+
 public class Trading {
 
 	private Product product;
@@ -8,4 +13,36 @@ public class Trading {
 
 	}
 
+	public int bidTradingValue() throws FileNotFoundException {
+
+		System.out.println("Order an item \n");
+
+		System.out.println("Enter the item from the menu displayed above\n");
+
+		Scanner sc = new Scanner(System.in);
+
+		String orderName=sc.next();
+
+		int tradingValue=0;
+
+		File file = new File("resources/UserProduct.txt");
+
+		Scanner tradingFile = new Scanner(file);
+
+		while(tradingFile.hasNextLine())
+		{
+			String productArray[] = tradingFile.nextLine().split(":");
+			if(productArray[1].equals(orderName))
+			{
+				System.out.println(productArray[0]);
+				tradingValue++;
+			}
+
+		}
+
+
+		System.out.println("no of users ordered that dish is " + tradingValue);
+
+		return 0;
+	}
 }
